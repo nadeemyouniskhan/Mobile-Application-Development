60abdb898d32c62a2a9f807b6300d4bb30723309
muhammadnadeem074 <fa18-bse-074@cuiatk.edu.pk>
2021-10-04 12:50:57 +0500

    Add Theory Assignment(Relative and constraint layout) # 1 upload

---
+++ Assignment Layout/Constraints_layout_Agn/app/src/main/java/com/example/constraints_layout_agn/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.constraints_layout_agn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
